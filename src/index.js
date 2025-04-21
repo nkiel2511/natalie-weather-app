ad295ba9152fa4c098ef876992a652487a1051b8
nkiel2511 <natalie.kiel@btinternet.com>
2025-04-21 15:31:42 +0100

    Added function to search for city name, make API call and pull back current temperature updating UI on page

--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
+function updateData(response) {
+  let tempElement = document.querySelector("#temp");
+  let temp = response.data.temperature.current;
+  tempElement.innerHTML = Math.round(temp);
+
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+}
+
+function searchCity(city) {
+  let apiKey = "7ff3c5ef4330987abab23faeo62t9ee4";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(updateData);
+}
+
 function search(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", search);
+
+searchCity("Glasgow");
